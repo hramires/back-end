d52b274a55677761c43ecf0cd23b54f57b78c7c4
Arthur Amestrete <Arthur.amestrete@Dellteam.com>
2023-04-01 10:26:26 -0300

    route fix

--- app.js
+++ app.js
@@ -18,18 +18,7 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/users", userRouter);
-
-// Route to fetch all users from the database
-app.get("/", async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
-});
+app.use("/user", userRouter);
 
 db.authenticate()
   .then(() => {
