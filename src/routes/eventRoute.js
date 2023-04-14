99aa2beba9daeab596b58ce869a39c9be853f738
Francisco Lisboa <franciscolisboa03@gmail.com>
2023-04-14 15:17:15 -0300

    Remoção de duas declaracoes de router no eventRoute

--- src/routes/eventRoute.js
+++ src/routes/eventRoute.js
@@ -1,7 +1,6 @@
 const Event = require("../models/event");
 const express = require("express");
 const eventController = require("../controllers/eventController");
-const router = require("./categoryRoute");
 
 const router = express.Router();
 
