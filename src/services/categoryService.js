afb27d2730039716479fa0b225b65c9e2fd52da7
Arthur Amestrete <Arthur.amestrete@Dellteam.com>
2023-05-06 16:52:15 -0300

    catch(error) return fix

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -4,16 +4,23 @@ const Category = require("../models/category");
 async function create(req, res, next) {
   try {
     const { name } = req.body;
-    if(name == ""){
-      return { status: 400, data: { "error": "Nome da categoria não deve ser vazio." } };
+    if (name == "") {
+      return {
+        status: 400,
+        data: { error: "Nome da categoria não deve ser vazio." },
+      };
     }
-    if(name.length <=3){
-      return { status: 400, data: { "error": "Nome da categoria deve possuir 4 ou mais caracteres." } };
+    if (name.length <= 3) {
+      return {
+        status: 400,
+        data: { error: "Nome da categoria deve possuir 4 ou mais caracteres." },
+      };
     }
     const category = await Category.create({ name });
     return { status: 200, data: { category } };
   } catch (error) {
-    errorHandler(error, req, res, next);
+    //errorHandler(error, req, res, next);
+    return { status: 500, data: { error: "Internal Server Error" } };
   }
 }
 
@@ -22,7 +29,8 @@ async function getAll(req, res, next) {
     const categories = await Category.findAll();
     return { status: 200, data: { categories } };
   } catch (error) {
-    errorHandler(error, req, res, next);
+    //errorHandler(error, req, res, next);
+    return { status: 500, data: { error: "Internal Server Error" } };
   }
 }
 
@@ -36,7 +44,8 @@ async function getById(req, res, next) {
       return { status: 404, data: { category } };
     }
   } catch (error) {
-    errorHandler(error, req, res, next);
+    //errorHandler(error, req, res, next);
+    return { status: 500, data: { error: "Internal Server Error" } };
   }
 }
 
@@ -46,11 +55,19 @@ async function update(req, res, next) {
     const category = await Category.findByPk(id);
     if (category) {
       const { name } = req.body;
-      if(name == ""){
-        return { status: 400, data: { "error": "Nome da categoria não deve ser vazio." } };
+      if (name == "") {
+        return {
+          status: 400,
+          data: { error: "Nome da categoria não deve ser vazio." },
+        };
       }
-      if(name.length <=3){
-        return { status: 400, data: { "error": "Nome da categoria deve possuir 4 ou mais caracteres." } };
+      if (name.length <= 3) {
+        return {
+          status: 400,
+          data: {
+            error: "Nome da categoria deve possuir 4 ou mais caracteres.",
+          },
+        };
       }
       await category.update({ name: name });
       return { status: 200, data: { category } };
@@ -58,7 +75,8 @@ async function update(req, res, next) {
       return { status: 404, data: { error: "Category not found" } };
     }
   } catch (error) {
-    errorHandler(error, req, res, next);
+    //errorHandler(error, req, res, next);
+    return { status: 500, data: { error: "Internal Server Error" } };
   }
 }
 
@@ -73,7 +91,8 @@ async function remove(req, res, next) {
       return { status: 404, data: { error: "Category not found" } };
     }
   } catch (error) {
-    errorHandler(error, req, res, next);
+    //errorHandler(error, req, res, next);
+    return { status: 500, data: { error: "Internal Server Error" } };
   }
 }
 
