b135032a26bd15b14371e8355fb03d7f73369995
Sofia Arend <sofia.arend@edu.pucrs.br>
2023-05-07 19:24:33 -0300

    fix dataPreload and remove location from event

--- src/config/databasePreLoad.js
+++ src/config/databasePreLoad.js
@@ -15,25 +15,21 @@ async function preloadData() {
   try {
     const categoryData = [
       {
+        id: 1,
         name: "Cervejarias",
-      },
-    ];
-
-    const placeCategoryData = [
-      {
-        category_id: 1,
-        place_id: 1,
       },
     ];
 
     const photoData = [
       {
+        id: 1,
         photo_url: "photoUrl",
         description: "Foto do lugar que vamos ",
       },
     ];
     const regionData = [
       {
+        id: 1,
         city: "Caxias do Sul",
         description:
           "Grande do Sul com maior número de habitantes, Caxias do Sul está no ranking dos 100 melhores municípios brasileiros para se viver, sendo a primeira cidade gaúcha a liderar essa lista.",
@@ -42,11 +38,10 @@ async function preloadData() {
 
     const placeData = [
       {
+        id: 1,
         region_id: 1,
-        //placeCategory_id: 1,
-        place_id: 1,
         name: "Casa da Edição",
-        opening_hour: "08:00",
+        openingHour: "08:00",
         contact: "+5551998231918",
         latitude: "55",
         longitude: "11",
@@ -55,9 +50,17 @@ async function preloadData() {
       },
     ];
 
+    const placeCategoryData = [
+      {
+        id: 1,
+        category_id: 1,
+        place_id: 1,
+      },
+    ];
+
     const roadmapData = [
       {
-        roadmapPlace_id: 1,
+        id: 1,
         name: "Esse só tem um local de visitação",
         description: "Segue reto, primeira e unica parada é lá",
       },
@@ -65,6 +68,7 @@ async function preloadData() {
 
     const roadmapPlaceData = [
       {
+        id: 1,
         roadmap_id: 1,
         place_id: 1,
         order: 1,
@@ -73,18 +77,18 @@ async function preloadData() {
 
     const eventData = [
       {
+        id: 1,
         place_id: 1,
         name: "SHOW PEDRO SAMPAIO - CAXIAS DO SUL",
         description: "Aqui é festa =)",
-        date: new Date("2023-03-30").toISOString(),
-        time: "22:00",
-        location:
-          "Pavilhões da Festa da Uva. R. Ludovíco Cavinato, 1431 - Nossa Sra. da Saúde, Caxias do Sul - RS",
+        startDate: new Date("2023-03-25").toISOString(),
+        endDate: new Date("2023-03-30").toISOString(),
+        openingHour: "22:00",
       },
     ];
 
     // Cleaning data
-    cleanDatabase();
+    await cleanDatabase();
 
     await Category.bulkCreate(categoryData);
     await Region.bulkCreate(regionData);
